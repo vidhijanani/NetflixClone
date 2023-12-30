695bca39c3c67275e4c59a37664849f078e59658
vidhijanani <vidhijanani6@gmail.com>
2023-12-30 11:12:37 +0530

    i am adding work files with this repo

---
+++ main.js
@@ -0,0 +1,9 @@
+  function showPara(vidhi) {
+
+            var paragraph = document.getElementById('demo');
+            if (paragraph.style.display === 'none' || paragraph.style.display === '') {
+                paragraph.style.display = 'block';
+            } else {
+                paragraph.style.display = 'none';
+            }
+        }
